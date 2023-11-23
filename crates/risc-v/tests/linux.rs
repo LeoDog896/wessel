9831f6e61a89f05e82d5657eed77e51fe452a4c9
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-11-23 17:47:49 -0500

    test: run commands

--- crates/risc-v/tests/linux.rs
+++ crates/risc-v/tests/linux.rs
@@ -1,9 +1,10 @@
 use risc_v::terminal::Terminal;
 use risc_v::Emulator;
 
-use std::fs;
+use std::{fs, thread};
 use std::num::NonZeroU8;
 use std::sync::{Arc, Mutex};
+use std::time::Duration;
 
 struct MultiThreadedTerminal {
     input_buffer: Arc<Mutex<Vec<NonZeroU8>>>,
@@ -14,21 +15,41 @@ fn str_to_vec(str: &str) -> Vec<NonZeroU8> {
     str.bytes().map(|b| NonZeroU8::new(b).unwrap()).collect()
 }
 
-fn is_sub<T: PartialEq>(haystack: &[T], needle: &[T]) -> bool {
+fn is_sub<T: PartialEq>(haystack: &[T], needle: &[T], window: usize) -> bool {
     haystack
         .windows(needle.len())
         .rev()
-        .take(10)
+        .take(window)
         .any(|c| c == needle)
 }
 
-fn wait_until(buffer: &Arc<Mutex<Vec<NonZeroU8>>>, values: Vec<NonZeroU8>) {
+fn wait_until(
+    buffer: &Arc<Mutex<Vec<NonZeroU8>>>,
+    values: Vec<NonZeroU8>,
+    duration: Duration,
+    window: usize
+) {
+    let start = std::time::Instant::now();
     loop {
         let output_buffer = buffer.lock().unwrap();
-        if is_sub(&output_buffer, &values) {
+        if is_sub(&output_buffer, &values, window) {
             break;
         }
+        if start.elapsed() > duration {
+            let mut buf_data = output_buffer.iter().map(|v| v.get()).rev().take(window + values.len()).collect::<Vec<u8>>();
+            buf_data.reverse();
+            panic!(
+                "Timeout: {}\n\n{}",
+                std::str::from_utf8(&values.iter().map(|v| v.get()).collect::<Vec<u8>>()).unwrap(),
+                std::str::from_utf8(&buf_data).unwrap()
+            );
+        }
     }
+}
+
+fn write_to_buffer(buffer: &Arc<Mutex<Vec<NonZeroU8>>>, values: Vec<NonZeroU8>) {
+    let mut output_buffer = buffer.lock().unwrap();
+    output_buffer.extend(values);
 }
 
 impl Terminal for MultiThreadedTerminal {
@@ -37,10 +58,7 @@ impl Terminal for MultiThreadedTerminal {
     }
 
     fn get_input(&mut self) -> Option<NonZeroU8> {
-        match !self.input_buffer.lock().unwrap().is_empty() {
-            true => Some(self.input_buffer.lock().unwrap().remove(0)),
-            false => None,
-        }
+        self.input_buffer.lock().unwrap().pop()
     }
 }
 
@@ -56,8 +74,8 @@ fn main() {
     let local_output_buffer = output_buffer.clone();
     std::thread::spawn(move || {
         let mut emulator = Emulator::new(Box::new(MultiThreadedTerminal {
-            input_buffer: local_input_buffer.clone(),
-            output_buffer: local_output_buffer.clone(),
+            input_buffer: local_input_buffer,
+            output_buffer: local_output_buffer,
         }));
         emulator.setup_program(elf_contents);
 
@@ -68,7 +86,35 @@ fn main() {
 
     wait_until(
         &output_buffer.clone(),
-        str_to_vec("Please press Enter to activate this console."),
+        str_to_vec("Please press Enter to activate this console"),
+        Duration::from_secs(60),
+        10,
     );
-    println!("Done booting...");
+
+    let write_to_buffer = |value: &str| {
+        write_to_buffer(&input_buffer.clone(), str_to_vec(value));
+    };
+
+    // TODO: wait with clock cycles instead of sleeping
+
+    let typewriter = |value: &str, delay: Duration| {
+        for c in value.chars() {
+            write_to_buffer(&c.to_string());
+            thread::sleep(delay);
+        }
+    };
+    
+    thread::sleep(Duration::from_millis(500));
+
+    write_to_buffer("\n");
+
+    wait_until(&output_buffer.clone(), str_to_vec("/ #"), Duration::from_secs(3), 10);
+
+    thread::sleep(Duration::from_millis(500));
+
+    typewriter("ls\n", Duration::from_millis(50));
+
+    thread::sleep(Duration::from_millis(500));
+
+    wait_until(&output_buffer.clone(), str_to_vec("lost+found"), Duration::from_secs(3), 500);
 }
